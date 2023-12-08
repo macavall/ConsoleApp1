88404d6debc1e33dccfc40d372928ad547940136
Matthew Cavallo <macavall@microsoft.com>
2023-12-08 13:19:21 -0600

    Completed looping test

--- Program.cs
+++ Program.cs
@@ -1,68 +1,94 @@
-﻿using System;
-using System.Threading.Tasks;
-static async Task Main()
+﻿namespace ConsoleApp1
 {
-    await Console.Out.WriteLineAsync("Starting");
-
-    var myByte = new ByteArrayCreator();
-
-    Console.ReadLine();
-}
-
-public class ByteArrayCreator : IDisposable
-{
-    private byte[] byteArray;
-    private bool disposed;
-
-    public ByteArrayCreator()
+    internal class Program
     {
-        // Create a byte array of size 100 MB
-        byteArray = new byte[100 * 1024 * 1024];
-
-        // Fill the byte array with some data
-        for (int i = 0; i < byteArray.Length; i++)
+        static async Task Main(string[] args)
         {
-            byteArray[i] = 1;
+            Console.WriteLine("Starting");
+
+            var myByte = new ByteArrayCreator();
+
+            await myByte.WaitForDispose();
+
+            await Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(1);
+                Thread.Sleep(6000);
+
+                for(int i = 0; i < 15; i++)
+                {
+                    _ = Task.Factory.StartNew(async () =>
+                    {
+                        var myByte = new ByteArrayCreator();
+
+                        await myByte.WaitForDispose();
+                    });
+                }
+            });
+
+            Console.ReadLine();
+        }
+    }
+
+    public class ByteArrayCreator : IDisposable
+    {
+        private byte[] byteArray;
+        private bool disposed;
+
+        public ByteArrayCreator()
+        {
+            // Create a byte array of size 100 MB
+            byteArray = new byte[100 * 1024 * 1024];
+
+            // Fill the byte array with some data
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                byteArray[i] = 1;
+            }
+
+            Console.WriteLine("Byte array created and filled with data.");
         }
 
-        Console.WriteLine("Byte array created and filled with data.");
-    }
-
-    public async Task WaitForDispose()
-    {
-        // Wait for 30 seconds
-        await Task.Delay(TimeSpan.FromSeconds(30));
-
-        Console.WriteLine("30 seconds have passed. The byte array can now be disposed of.");
-
-        Dispose();
-    }
-
-    ~ByteArrayCreator()
-    {
-        Dispose(false);
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposed)
+        public async Task WaitForDispose()
         {
-            return;
+            Console.WriteLine("Creating ByteArray");
+
+            // Wait for 30 seconds
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            Console.WriteLine("30 seconds have passed. The byte array can now be disposed of.");
+
+            Dispose();
         }
 
-        if (disposing)
+        ~ByteArrayCreator()
         {
-            // Dispose managed objects
-            byteArray = null;
+            Dispose(false);
         }
 
-        // Dispose unmanaged objects
-        disposed = true;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // Dispose managed objects
+                byteArray = null;
+            }
+
+            // Dispose unmanaged objects
+            disposed = true;
+        }
     }
+
+
 }
